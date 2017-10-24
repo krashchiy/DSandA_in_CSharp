76802ebc494f9b4f4c8ba13bea363ea5ebc077bc
Aaron Clark <akclark@atbashservices.com>
2017-10-24 11:12:54 -0500

    Fixed Bubblesort for strings/int sorting.

--- Algorithms/Sorting/BubbleSort/BubbleSort/Program.cs
+++ Algorithms/Sorting/BubbleSort/BubbleSort/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Runtime.Serialization.Formatters;
 
 namespace BubbleSort
 {
@@ -8,20 +7,31 @@ namespace BubbleSort
     {
         static void Main(string[] args)
         {
-            List<int> myInts = new List<int>() {105, 21, 13, 41, 15, 61, 17, 81, 19, 101, 99, 112, 1};
+            List<string> myStrings = new List<string> {"APE", "APPLE", "A", "B", "C","apple", "ape", "F","E", "o","n","m","l","k","j","i","h","g","f","e","d","c","b","a","9","8","7","6","5","4","3","2","1" };
+            
 
-            List<int> result = BubbleSort(myInts);
+            List<string> StringsResult = BubbleSortStrings(myStrings);
 
-            foreach (int number in result)
+            foreach (string character in StringsResult)
             {
-                Console.WriteLine(number.ToString());
+                Console.Write(character + " ");
             }
         
             Console.ReadLine();
 
+
+            List<int> myInts = new List<int> {16, 2, 128, 512, 1024, 256, 63, 32, 8, 4};
+
+            List<int> IntsResult = BubbleSortInts(myInts);
+            foreach (int number in IntsResult)
+            {
+                Console.Write(number + " ");
+            }
+            Console.ReadLine();
+
         }
 
-        private static List<int> BubbleSort(List<int> myInts)
+        private static List<int> BubbleSortInts(List<int> myInts)
         {
             while (1==1)
             {
@@ -53,6 +63,40 @@ namespace BubbleSort
             }
 
             return myInts;
+        }
+        private static List<string> BubbleSortStrings(List<string> myStrings)
+        {
+            while (1 == 1)
+            {
+                bool Swapping = true;
+
+                // Repeat forever:
+                while (Swapping)
+                {
+                    bool swappedThisLoop = false;
+
+                    // Scan through the list
+                    for (int i = 0; i < myStrings.Count - 1; i++)
+                    {
+                        // If two adjacent items are out of order, swap them
+                        if (string.Compare(myStrings[i], myStrings[i + 1]) < 0)
+                        {
+                            string temp = myStrings[i];
+                            myStrings[i] = myStrings[i + 1];
+                            myStrings[i + 1] = temp;
+                            swappedThisLoop = true;
+                        }
+
+                    }
+
+                    // If you finish the scan without a swap, you're done
+                    Swapping = swappedThisLoop;
+                }
+
+                break;
+            }
+
+            return myStrings;
         }
     }
 }
