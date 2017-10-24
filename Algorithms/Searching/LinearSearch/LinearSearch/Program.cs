e8b7904d21e1b25c7cc63a80605e0f3d67439812
Aaron Clark <akclark@atbashservices.com>
2017-10-24 15:24:07 -0500

    Added ints to LinearSearch

--- Algorithms/Searching/LinearSearch/LinearSearch/Program.cs
+++ Algorithms/Searching/LinearSearch/LinearSearch/Program.cs
@@ -6,11 +6,12 @@ namespace LinearSearch
     {
         static void Main(string[] args)
         {
-            int[] testData = new int[] {8, 4, 16, 256, 2, 64, 128, 32};
+            int[] testData = new int[] { 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144, 524288 };
 
-            bool result = LinearSearchSuccess(testData, 32);
+            bool result = LinearSearchSuccess(testData, 8192);
 
             Console.WriteLine(result ? "Found it!" : "No Luck!");
+            Console.ReadLine();
         }
 
         public static bool LinearSearchSuccess(int[] array, int target)
